fd8be64f10f441fb29e210d85ea445a22068545d
王勇 <wy19945372694@163.com>
2023-05-24 11:28:48 +0800

    update

--- src/main/java/com/example/demo/Controller/HelloController.java
+++ src/main/java/com/example/demo/Controller/HelloController.java
@@ -1,13 +1,34 @@
 package com.example.demo.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/666.jpg")
-    public String hello(){
-        return "666";
+//    @RequestMapping("/666.jpg")
+//    public String hello(){
+//        return "666";
+//    }
+
+    @RequestMapping(value = "/user",method = RequestMethod.GET)
+    public String getUser(){
+        return "GET-User";
+    }
+
+    @RequestMapping(value = "/user",method = RequestMethod.POST)
+    public String saveUser(){
+        return "POST-User";
+    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.PUT)
+    public String putUser(){
+        return "PUT-User";
+    }
+
+    @RequestMapping(value = "/user",method = RequestMethod.DELETE)
+    public String deleteUser(){
+        return "DELETE-User";
     }
 }
