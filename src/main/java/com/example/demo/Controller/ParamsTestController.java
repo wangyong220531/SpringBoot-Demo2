dda55baa2a0dcfca299518c227bf5bda8a8e8b93
王勇 <wy19945372694@163.com>
2023-05-24 20:10:09 +0800

    update

--- src/main/java/com/example/demo/Controller/ParamsTestController.java
+++ src/main/java/com/example/demo/Controller/ParamsTestController.java
@@ -12,7 +12,6 @@ public class ParamsTestController {
     public Map<String, Object> getCar(@PathVariable("id") Integer id,
                                       @PathVariable("username") String name,
                                       @PathVariable Map<String, String> pv) {
-
         Map<String, Object> map = new HashMap<>();
         map.put("id", id);
         map.put("name", name);
@@ -21,23 +20,27 @@ public class ParamsTestController {
     }
 
     @GetMapping("/getHeader")
-    public Map<String, Object> getHeader(
-            @RequestHeader("Authorization") String authorization,
-            @RequestHeader Map<String, String> headers) {
+    public Map<String, Object> getHeader(@RequestHeader("Authorization") String authorization,
+                                         @RequestHeader Map<String, String> headers) {
         Map<String, Object> map = new HashMap<>();
         map.put("authorization", authorization);
         map.put("headers", headers);
         return map;
     }
 
-    @GetMapping("/getVariable")
-    public Map<String, Object> getVariable(
-            @RequestParam("name") String paramName,
-            @RequestParam Map<String, String> params
-    ) {
+    @GetMapping("/getParams")
+    public Map<String, Object> getVariable(@RequestParam("name") String paramName,
+                                           @RequestParam Map<String, String> params) {
         Map<String, Object> map = new HashMap<>();
         map.put("paramName", paramName);
         map.put("params", params);
+        return map;
+    }
+
+    @GetMapping("/getCookie")
+    public Map<String, Object> getCookie(@CookieValue("_ga") String _ga) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("_ga", _ga);
         return map;
     }
 }
