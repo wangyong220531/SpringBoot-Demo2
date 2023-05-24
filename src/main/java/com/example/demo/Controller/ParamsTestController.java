bfb4f60ae9333e9a0b2833f7e8f0dea5c3a0a20e
王勇 <wy19945372694@163.com>
2023-05-24 20:09:08 +0800

    update

--- src/main/java/com/example/demo/Controller/ParamsTestController.java
+++ src/main/java/com/example/demo/Controller/ParamsTestController.java
@@ -8,21 +8,34 @@ import java.util.Map;
 @RestController
 public class ParamsTestController {
 
-    @GetMapping("/getCar")
+    @GetMapping("/getVariable")
     public Map<String, Object> getCar(@PathVariable("id") Integer id,
                                       @PathVariable("username") String name,
-                                      @PathVariable Map<String, String> pv,
-                                      @RequestHeader("Authorization") String authorization,
-                                      @RequestHeader Map<String, String> headers,
-                                      @RequestParam("name") String paramName,
-                                      @RequestParam Map<String, String> params) {
+                                      @PathVariable Map<String, String> pv) {
 
         Map<String, Object> map = new HashMap<>();
         map.put("id", id);
         map.put("name", name);
         map.put("pv", pv);
+        return map;
+    }
+
+    @GetMapping("/getHeader")
+    public Map<String, Object> getHeader(
+            @RequestHeader("Authorization") String authorization,
+            @RequestHeader Map<String, String> headers) {
+        Map<String, Object> map = new HashMap<>();
         map.put("authorization", authorization);
         map.put("headers", headers);
+        return map;
+    }
+
+    @GetMapping("/getVariable")
+    public Map<String, Object> getVariable(
+            @RequestParam("name") String paramName,
+            @RequestParam Map<String, String> params
+    ) {
+        Map<String, Object> map = new HashMap<>();
         map.put("paramName", paramName);
         map.put("params", params);
         return map;
